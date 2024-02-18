1e65ad399fdcfc593573b2696bccfdaa860a7eff
prathamlangi <146056601+prathamlangi@users.noreply.github.com>
2024-02-18 15:00:41 +0530

    Add files via upload

---
+++ first attempt.sql
@@ -0,0 +1,6 @@
+select customer.customer_id,customer.first_name,customer.last_name,sum(invoice.total) as total	
+from customer
+join invoice on customer.customer_id=invoice.customer_id
+group by customer.customer_id
+order by total desc
+limit 1
